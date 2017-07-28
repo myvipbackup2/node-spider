7e947b2327756ef640077a60c1396d4b5d7a2237
李子翔 <zixiangli@ctrip.com>
2017-07-28 09:07:26 +0800

    init project fetch MDN

--- douban/index.js
+++ douban/index.js
@@ -4,7 +4,25 @@ const jieba = require('nodejieba');
 
 async function fetchSingleDoubanList(start) {
 
-    let res = await axios.get(`https://www.douban.com/group/shanghaizufang/`);
+    let res = await axios.get(`https://www.douban.com/group/shanghaizufang/discussion?start=${start}`);
     let htmlText = res.data;
+
+    const $ = cheerio.load(htmlText);
+    const rs = $('a[title]');
+
+    const resultList = [];
+
+    for (let i = 0, len = rs.length; i < len; i++) {
+        resultList.push({
+            title: rs.eq(i).attr('title'),
+            url: rs.eq(i).attr('href')
+        })
+    }
+
+    return resultList;
+
+}
+
+async function fetchSingleDoubanTopic(url) {
 
 }
